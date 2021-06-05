93dd4a655ad8c26c16b2812f0fb4cd8dbd70d727
Alex <33379584+alexyao2015@users.noreply.github.com>
2021-06-05 09:44:21 -0500

    Ignore unmount failure

--- root/etc/cont-init.d/50-fix-mnt-dir.sh
+++ root/etc/cont-init.d/50-fix-mnt-dir.sh
@@ -6,4 +6,4 @@ program_name="fix-mnt-dir"
 mkdir -p "${FINAL_MOUNT_DIR}"
 
 # Need to ensure that media isn't currently mounted before mounting
-fusermount -uz "${FINAL_MOUNT_DIR}" > /dev/null 2>&1
+fusermount -uz "${FINAL_MOUNT_DIR}" > /dev/null 2>&1 || true
