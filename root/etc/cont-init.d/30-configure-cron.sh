898b16fb17ed04bad95127f37cab6421157f98f1
Alex <33379584+alexyao2015@users.noreply.github.com>
2022-02-15 12:21:16 -0600

    Fix cron

--- root/etc/cont-init.d/30-configure-cron.sh
+++ root/etc/cont-init.d/30-configure-cron.sh
@@ -4,8 +4,8 @@ program_name="configure-cron"
 
 echo "Configuring cron scripts..." | info "[${program_name}] "
 
-echo "${CRON_CLOUDUPLOAD_TIME} s6-svc -o /run/service/script-cloudupload
-${CRON_RMDELETE_TIME} s6-svc -o /run/service/script-rmlocal
-${CRON_DEDUPE_TIME} s6-svc -o /run/service/script-dedupe
-${CRON_MIRROR_TIME} s6-svc -o /run/service/script-mirror
-${CRON_EMPTY_TRASH_TIME} s6-svc -o /run/service/script-emptytrash" > /etc/crontabs/root
+echo "${CRON_CLOUDUPLOAD_TIME} /command/s6-svc -o /run/service/script-cloudupload
+${CRON_RMDELETE_TIME} /command/s6-svc -o /run/service/script-rmlocal
+${CRON_DEDUPE_TIME} /command/s6-svc -o /run/service/script-dedupe
+${CRON_MIRROR_TIME} /command/s6-svc -o /run/service/script-mirror
+${CRON_EMPTY_TRASH_TIME} /command/s6-svc -o /run/service/script-emptytrash" > /etc/crontabs/root
